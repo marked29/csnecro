655c320eb24d94e3e958444bd7135e5bea680e1c
Parhosha <42674033+Parhosha@users.noreply.github.com>
2021-08-24 12:35:33 +0300

    Please enter the commit message for your changes. Lines starting

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { Route, BrowserRouter } from 'react-router-dom';
-import { Header } from './features/header/Header';
+import Header from './features/header/Header';
 import './App.css';
 
 const useStyles = makeStyles((theme) => ({
--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,14 +1,72 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
+// import { Button } from '@material-ui/core';
+// import { FC } from 'react';
+// import { Link } from 'react-router-dom';
+// import styles from './Header';
 
-export const Header: FC = () => {
+// export const Header: FC = () => {
+//   const style = styles();
+
+//   return (
+//     <div className={style.header}>
+//       <div className={style.leftHead}>
+//         <Link to="/coinflip"> coinflip </Link>
+//         <Link to="/jackpot"> jackpot </Link>
+//       </div>
+//       <div className={style.rightHead}>
+//         <Link to="/fairness"> fairness </Link>
+//         <Link to="/support"> support </Link>
+//         <Link to="/faq"> faq </Link>
+//         <Link to="/terms"> terms </Link>
+//         <Button> ENG </Button>
+//         <Button> Sound </Button>
+//         <Button> LOGIN </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <ul>
-        <Link to="/coinflip"> coinflip </Link>
-        <Link to="/jackpot"> jackpot </Link>
-        <Link to="/faq"> faq </Link>
-      </ul>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
-};
+}
