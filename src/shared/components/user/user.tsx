4a14ca7992cbbfb2669ba3ab3851727a8d4d042e
Mark Misra <markmisra@gmail.com>
2021-09-07 01:25:55 +0300

    Add refactorring for jackpot

--- src/shared/components/user/user.tsx
+++ src/shared/components/user/user.tsx
@@ -1,25 +1,31 @@
 import { FC } from 'react';
 
+import cn from 'classnames';
+
 import style from './user.module.sass';
 
 type UserProps = {
   className?: string;
   winnerLabel?: boolean;
+  hiddenSpan?: boolean;
 };
 
-const User: FC<UserProps> = ({ className, winnerLabel }) => {
-  return (
-    <div className={className}>
-      <img className={style.avatar} src="/img/avatar.png" alt="" />
-      <span className={style.levelLabel}>27</span>
+const User: FC<UserProps> = ({ className, winnerLabel, hiddenSpan }) => {
+  const compoundClassName = cn(style.root, className);
 
-      <span className={style.userName}>Username</span>
-      <span className={style.message}> Lorem ipsum ipsum</span>
-      {winnerLabel && (
-        <div className={style.winner}>
-          <p>WINNER</p>
-        </div>
-      )}
+  return (
+    <div className={compoundClassName}>
+      <div className={style.userInfo}>
+        <img className={style.avatar} src="/img/avatar.png" alt="avatar" />
+        <span className={style.levelLabel}>27</span>
+        {!hiddenSpan && (
+          <>
+            <span className={style.userName}>Username</span>
+            <span className={style.message}> Lorem ipsum ipsum</span>
+          </>
+        )}
+      </div>
+      {winnerLabel && <div className={style.winner}>WINNER</div>}
     </div>
   );
 };
